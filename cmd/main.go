06c5fb1c856a06e7629193c0665d0dee7e62c47e
12262004-m <nov173@yandex.ru>
2025-12-24 13:46:18 +0300

    add main.go

---
+++ cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"delivery-tracker-go/internal/models"
+	"log/slog"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	cnf := loadConfig()
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	dbModel, err := models.InitDB(cnf.DB)
+	if err != nil {
+		slog.Error("Failed to initialize DB", "error", err)
+		os.Exit(1)
+	}
+	slog.Error("DB initialized successfully", "error", err)
+
+	RegisterCustomValidators()
+
+	h := NewHandler(dbModel)
+
+	router := gin.Default()
+	if err := loadTemplates(router); err != nil {
+		slog.Error("Failed to load templates", "error", err)
+		os.Exit(1)
+	}
+
+	setupRoutes(router, h)
+	slog.Info("Server starting", "url", "http://localhost:"+cnf.Port)
+	router.Run(":" + cnf.Port)
+
+}
